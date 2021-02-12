1bec99a8690d500def14b245c1f2b3736ae1c749
HEZHO <zhouh@smu.edu>
2021-02-13 00:59:39 +0800

    User Gin

--- main.go
+++ main.go
@@ -1,31 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
+	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-func sayHello(w http.ResponseWriter, r *http.Request){
-	// 解析模板
-	t,_ := template.ParseFiles("./hello.tmpl");
-
-	name := "HeZho"
-	// 渲染模板
-	err := t.Execute(w, name);
-	if err != nil {
-		fmt.Println("Render template failed, err:%v", err);
-		return
-	}
-
+func sayHello(c *gin.Context){
+	c.JSON(http.StatusOK, gin.H{
+		"message": "HEZHO, CHARGE !!!!",
+	})
 }
 
+// 使用 REST 模式来写
 func main() {
-	http.HandleFunc("/", sayHello)
-	err := http.ListenAndServe(":9000", nil)
-	if err != nil{
-		fmt.Println("HTTP server start failed, err:%v", err)
-		return
-	}
+	r := gin.Default()
+	r.GET("/", sayHello)
+	r.Run(":9000")
 }
 
