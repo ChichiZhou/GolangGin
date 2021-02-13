881b7ddaa4aab3320b75c385ea5b3932c01b89ad
HEZHO <zhouh@smu.edu>
2021-02-13 11:06:48 +0800

    Load Static Resource

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ func sayHello(c *gin.Context){
 }
 
 func sayHi(c *gin.Context){
-
 	// 模板渲染
 	c.HTML(http.StatusOK, "users/index.tmpl", gin.H{
 		"title":"users/index",
@@ -23,6 +22,9 @@ func sayHi(c *gin.Context){
 // 使用 REST 模式来写
 func main() {
 	r := gin.Default()
+	// 在解析模板之前，要先加载静态文件
+	// 这里的静态文件包括：css, js, 图片
+	r.Static("/statics", "./statics")  // 每一个 / 和 . 都不能少
 	// 模板解析
 	r.LoadHTMLFiles("templates/posts/index.tmpl", "templates/users/index.tmpl")
 	r.GET("/posts/index", sayHello)
