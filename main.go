c0dec063487efafd290095149bd3426d2b13f333
HEZHO <zhouh@smu.edu>
2021-02-13 14:26:29 +0800

    No need to pass parameter to html

--- main.go
+++ main.go
@@ -15,10 +15,16 @@ func sayHello(c *gin.Context){
 func sayHi(c *gin.Context){
 	// 模板渲染
 	// gin.H 是一个 map[string]interface()
-
 	c.HTML(http.StatusOK, "users/index.tmpl", gin.H{
 		"title":"users/index",
 	})
+}
+
+func helloWorld(c *gin.Context){
+	// 模板渲染
+	// 如果不向模板里传入数据，则可以将 gin.H 设为nil
+	// 由于 hello 是唯一的，所以不需要用 define
+	c.HTML(http.StatusOK, "hello.html", nil)
 }
 
 // 使用 REST 模式来写
@@ -28,9 +34,11 @@ func main() {
 	// 这里的静态文件包括：css, js, 图片
 	r.Static("/statics", "./statics")  // 每一个 / 和 . 都不能少
 	// 模板解析
-	r.LoadHTMLFiles("templates/posts/index.tmpl", "templates/users/index.tmpl")
+	r.LoadHTMLFiles("templates/posts/index.tmpl", "templates/users/index.tmpl", "templates/posts/hello.html")
 	r.GET("/posts/index", sayHello)
 	r.GET("/users/index", sayHi)
+	r.GET("/posts/hello", helloWorld)
+
 	r.Run(":9000")
 }
 
